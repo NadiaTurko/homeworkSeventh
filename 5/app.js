9734315cb1c780eae373c8c141240861686a837d
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-06-07 19:19:51 +0300

    Add files via upload

---
+++ 5/app.js
@@ -0,0 +1,18 @@
+// 7-5.
+// Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення
+// "I was pressed!", при наведенні на кнопку виводитиме повідомлення "Mouse on me!",
+// а при відведенні курсора миші виводитиме повідомлення "Mouse is not on me!".
+const btn = document.querySelector("button");
+const text = document.createElement("th");
+text.appendChild.btn;
+btn.addEventListener("click", function (event) {
+  text.innerHTML = "I was pressed!";
+});
+
+btn.addEventListener("mouseover", function (event) {
+  text.innerHTML = "Mouse on me!";
+});
+
+btn.addEventListener("mouseout", function (event) {
+  text.innerHTML = "Mouse is not on me!";
+});
